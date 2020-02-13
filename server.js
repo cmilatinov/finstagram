fbe8e2246a810428852db3dffa4c0bc7357e2995
Cristian Milatinov <milatinovcristian@gmail.com>
2020-02-13 18:13:26 -0500

    Host live server serving minified project

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+
+const port = 8081;
+app.listen(port);
+
+const port = process.env.SERVER_PORT || 8081;
+app.listen(port, _ => console.log(`Server started on port ${port}`));
