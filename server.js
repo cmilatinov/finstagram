d14132c248b3981f60391de0037c829105ef95b4
Cristian Milatinov <milatinovcristian@gmail.com>
2020-02-13 18:17:52 -0500

    Install required npm packages, fix live server script

--- server.js
+++ server.js
@@ -3,8 +3,5 @@ const app = express();
 
 app.use(express.static('dist'));
 
-const port = 8081;
-app.listen(port);
-
 const port = process.env.SERVER_PORT || 8081;
 app.listen(port, _ => console.log(`Server started on port ${port}`));
