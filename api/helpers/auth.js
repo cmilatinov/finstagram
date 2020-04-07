1aafc4f66e61a25ba8ded5bc23c7e00873624895
Cristian Milatinov <milatinovcristian@gmail.com>
2020-04-07 17:43:24 -0400

    Auth fix err message

--- api/helpers/auth.js
+++ api/helpers/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 module.exports = (req, res, next) => {
     passport.authenticate('local', (err, user, _) => {
         if(err || !user)
-            return res.status(HTTP_UNAUTHORIZED).json({ error: err.message || 'Please login to use this feature.' });
+            return res.status(HTTP_UNAUTHORIZED).json({ error: err ? err.message : 'Please login to use this feature.' });
         
         req.login(user, err => {
             if(err)
