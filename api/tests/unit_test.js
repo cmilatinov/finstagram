a1ab29faf5fcfe5036dc7f733800ece1c6c9f7ca
Cristian Milatinov <milatinovcristian@gmail.com>
2020-04-07 20:17:08 -0400

    Unit testing polish + posting bugfix + reduce font-size on post view

--- api/tests/unit_test.js
+++ api/tests/unit_test.js
@@ -1,16 +1,73 @@
-const axios = require('axios').create({baseURL: 'http://184.160.100.238:3000', withCredentials: true});
+const axios = require('axios').create({ baseURL: 'http://localhost:3000', withCredentials: true });
+const logger = require('./logger');
+const { SUCCESS, ERROR, WARNING } = logger;
 
-async function test(){
-    let res = await axios.get('/');
-    console.log(`Index route API \n${JSON.stringify(res.data)}`);
+async function execTest(testName, test, onSuccess = null) {
+    if(!test)
+        return logger.log(' Please provide a valid test.', WARNING);
 
+    console.log(` Executing test '${testName.brightYellow.bold}' ...\n`);
     
-    res = await axios.post('/users/login', {username: 'Gahyki',password: 'asdf1234'}, {withCredentials: true});
-    console.log(`\nLogin Test \n${JSON.stringify(res.data)}`);
+    try {
 
+        res = await (test());
+        logger.logObject(res.data);
+        console.log();
+        logger.log('Test Passed!', SUCCESS);
+        console.log('\n=================================================\n');
 
-    res = await axios.get('/users/2/posts', {withCredentials: true});
-    console.log(`\nUser Posts \n${JSON.stringify(res.data)}`);
+        if(onSuccess)
+            onSuccess(res);
+
+        return true;
+
+    } catch(err) {
+
+        logger.log(err.message, ERROR);
+        logger.log(err.stack, ERROR);
+        console.log();
+        logger.log('Test Failed!', ERROR);
+        console.log('\n=================================================\n');
+
+        return false;
+
+    }
 }
 
-test();
+async function execAllTests() {
+
+    const tests = [
+        { 
+            name: 'Index Route  -  /', 
+            test: _ => axios.get(`/`) 
+        },
+        {   
+            name: 'Login Route  -  /users/login', 
+            test: _ => axios.post(`/users/login`, { username: 'rednite', password: 'power336' }), 
+            onSuccess: res => axios.defaults.headers.common['Cookie'] = res.headers['set-cookie'][0].split(';')[0]
+        },
+        { 
+            name: 'User Posts Route  -  /users/2/posts', 
+            test: _ => axios.get(`/users/2/posts`)
+        }
+    ];
+
+    let testsPassed = 0;
+
+    console.time(' Running time');
+
+    console.log('\n=================================================\n');
+
+    for(let test of tests) {
+
+        let success = await execTest(test.name, test.test, test.onSuccess);
+        if(success)
+            testsPassed++;
+
+    }
+
+    console.timeEnd(' Running time');
+    console.log(`\n Tests passed: ${(testsPassed + '/' + tests.length).brightCyan.bold} (${((testsPassed * 100 / tests.length).toFixed(2) + '%').brightCyan.bold})`)
+}
+
+execAllTests();
