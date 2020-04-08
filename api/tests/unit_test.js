4d98bbc3b7dec9e13f52a9aac5f08edaad18115e
Jason <jason.k99@hotmail.com>
2020-04-07 20:13:10 -0400

    Added post information check unit test

--- api/tests/unit_test.js
+++ api/tests/unit_test.js
@@ -1,16 +1,24 @@
-const axios = require('axios').create({baseURL: 'http://184.160.100.238:3000', withCredentials: true});
+const axios = require('axios').create({ baseURL: 'http://184.160.100.238:3000', withCredentials: true });
 
-async function test(){
-    let res = await axios.get('/');
-    console.log(`Index route API \n${JSON.stringify(res.data)}`);
+async function test() {
+    try {
 
+        let res = await axios.get('/');
+        console.log(`Index route API \n${JSON.stringify(res.data)}`);
+
+        res = await axios.post('/users/login', { username: 'rednite', password: 'power336' });
+        console.log(`\nLogin Test \n${JSON.stringify(res.data)}`);
+        axios.defaults.headers.common['Cookie'] = res.headers['set-cookie'][0].split(';')[0];
     
-    res = await axios.post('/users/login', {username: 'Gahyki',password: 'asdf1234'}, {withCredentials: true});
-    console.log(`\nLogin Test \n${JSON.stringify(res.data)}`);
+        res = await axios.get('/users/2');
+        console.log(`\nUser Posts \n${JSON.stringify(res.data)}`);
 
+        res = await axios.get('/posts/76');
+        console.log(`\nPost Information \n${JSON.stringify(res.data)}`)
 
-    res = await axios.get('/users/2/posts', {withCredentials: true});
-    console.log(`\nUser Posts \n${JSON.stringify(res.data)}`);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 test();
